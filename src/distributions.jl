db80af68bfc6e1861231bb9800c05b34f75e7508
Xuan <tanqazx@gmail.com>
2023-01-18 22:18:58 -0500

    Add labeled categorical distribution.

--- src/distributions.jl
+++ src/distributions.jl
@@ -1,5 +1,6 @@
 ## Useful distributions ##
 export LabeledUniform, labeled_uniform, symbol_uniform
+export LabeledCategorical, labeled_categorical, symbol_categorical
 export RegionUniform, RegionUniform2D, RegionUniform3D
 export region_uniform_2D, region_uniform_3D
 
@@ -13,12 +14,16 @@ LabeledUniform() = LabeledUniform{Any}()
 (d::LabeledUniform)(args...) = Gen.random(d, args...)
 
 @inline Gen.random(::LabeledUniform{T}, labels::AbstractArray{<:T}) where {T} =
-    sample(labels)
+    rand(labels)
+@inline Gen.random(::LabeledUniform{T}, labels::AbstractSet{<:T}) where {T} =
+    rand(labels)
 @inline Gen.random(::LabeledUniform{T}, labels::Tuple{Vararg{<:T}}) where {T} =
     rand(labels)
 @inline Gen.logpdf(::LabeledUniform{T}, x::T, labels::ArrayOrTuple{<:T}) where {T} =
-    log(sum(x == l for l in labels) / length(labels))
-
+    log(sum(x == l for l in labels)) - log(length(labels))
+@inline Gen.logpdf(::LabeledUniform{T}, x::T, labels::AbstractSet{<:T}) where {T} =
+    x in labels ? -log(length(labels)) : -Inf
+    
 Gen.logpdf_grad(::LabeledUniform, x, labels) =
     (nothing, nothing)
 Gen.has_output_grad(::LabeledUniform) =
@@ -28,6 +33,30 @@ Gen.has_argument_grads(::LabeledUniform) =
 
 const labeled_uniform = LabeledUniform{Any}()
 const symbol_uniform = LabeledUniform{Symbol}()
+
+"Categorical distribution over an array of labels."
+struct LabeledCategorical{T} <: Gen.Distribution{T} end
+
+LabeledCategorical() = LabeledCategorical{Any}()
+
+(d::LabeledCategorical)(args...) = Gen.random(d, args...)
+
+@inline Gen.random(::LabeledCategorical{T}, labels::AbstractArray{<:T}, probs::AbstractArray{<:Real}) where {T} =
+    sample(vec(labels), Weights(vec(probs)))
+@inline Gen.logpdf(::LabeledCategorical{T}, x::T, labels::AbstractArray{<:T}, probs::AbstractArray{<:Real}) where {T} =
+    log(sum((x .== vec(labels)) .* probs))
+@inline function Gen.logpdf_grad(::LabeledCategorical{T}, x, labels::AbstractArray{<:T}, probs::AbstractArray{<:Real}) where {T}
+    grad = (x .== vec(labels)) .* (1. ./ vec(probs))
+    return (nothing, nothing, grad)
+end
+
+Gen.has_output_grad(::LabeledCategorical) =
+    false
+Gen.has_argument_grads(::LabeledCategorical) =
+    (false, true)
+
+const labeled_categorical = LabeledCategorical{Any}()
+const symbol_categorical = LabeledCategorical{Symbol}()    
 
 "Uniform distribution over geometric regions."
 struct RegionUniform{D} <: Gen.Distribution{Vector{Float64}} end
