e48ea2ebd9aa6259eb39f59428eec20efaf0bca2
Xuan <tanqazx@gmail.com>
2023-01-19 16:36:37 -0500

    Support fast biased `update` for SMCTrajectoryGF.

--- src/smc_trajectory_gf.jl
+++ src/smc_trajectory_gf.jl
@@ -39,6 +39,12 @@ end
     n_ula_iters::Int = 2
     "ULA proposal step size."
     ula_step_size::Float64 = 0.002
+    "How much to adjust reverse kernels towards initial proposal distribution."
+    weight_init::Float64 = 2.0
+    "Adjustment decay factor after each kernel application."
+    weight_init_decay::Float64 = 0.5
+    "Flag to use a fast & lower variance (but biased) implementation of update."
+    fast_update::Bool = true
 end
 
 function Gen.simulate(gen_fn::SMCTrajectoryGF{D}, args::Tuple) where {D}
@@ -48,12 +54,12 @@ function Gen.simulate(gen_fn::SMCTrajectoryGF{D}, args::Tuple) where {D}
     pf = pf_initialize(target_fn, args, EmptyChoiceMap(), n_particles)
     # Perform NMC and ULA iterations
     selection = AllSelection()
-    weight_init = 2.0
+    weight_init = gen_fn.weight_init
     for t in 1:gen_fn.n_nmc_iters
         pf_move_reweight!(pf, nmc_reweight, (selection,);
                           step_size=gen_fn.nmc_step_size,
                           target_init=:backward, weight_init)
-        weight_init *= 0.5
+        weight_init *= gen_fn.weight_init_decay
     end
     pf_move_reweight!(pf, ula_reweight, (selection, gen_fn.ula_step_size), 
                       gen_fn.n_ula_iters)
@@ -89,12 +95,12 @@ function Gen.generate(
     pf = pf_initialize(target_fn, args, EmptyChoiceMap(), n_particles)
     # Perform NMC and ULA iterations
     selection = AllSelection()
-    weight_init = 2.0
+    weight_init = gen_fn.weight_init
     for _ in 1:gen_fn.n_nmc_iters
         pf_move_reweight!(pf, nmc_reweight, (selection,);
                           step_size=gen_fn.nmc_step_size,
                           target_init=:backward, weight_init)
-        weight_init *= 0.5 # Gradually adjust backward kernel
+        weight_init *= gen_fn.weight_init_decay
     end
     pf_move_reweight!(pf, ula_reweight, (selection, gen_fn.ula_step_size), 
                       gen_fn.n_ula_iters)
@@ -118,39 +124,45 @@ function Gen.update(
     trace::SMCTrajectoryTrace{D}, args::Tuple,
     argdiffs::Tuple, constraints::ChoiceMap
 ) where {D}
+    gen_fn = get_gen_fn(trace)
     # Update chosen trace
     old_chosen_trace = trace.chosen_trace
     new_chosen_trace, _, retdiff, discard =
         update(old_chosen_trace, args, argdiffs, constraints)
-    # Infer how new chosen trace could have been sampled
-    selection = AllSelection()
-    bwd_trace, bwd_weight = new_chosen_trace, get_score(new_chosen_trace)
-    gen_fn = get_gen_fn(trace)
-    for _ in gen_fn.n_ula_iters # Apply backward ULA kernels
-        bwd_trace, inc_weight =
-            ula_reweight(bwd_trace, selection, gen_fn.ula_step_size)
-        bwd_weight += inc_weight
+    if gen_fn.fast_update # Avoid meta-inference 
+        new_chosen_weight = trace.chosen_weight -
+            get_score(old_chosen_trace) + get_score(new_chosen_trace)
+    else # Infer how new chosen trace could have been sampled
+        selection = AllSelection()
+        bwd_trace, bwd_weight = new_chosen_trace, get_score(new_chosen_trace)
+        gen_fn = get_gen_fn(trace)
+        for _ in gen_fn.n_ula_iters # Apply backward ULA kernels
+            bwd_trace, inc_weight =
+                ula_reweight(bwd_trace, selection, gen_fn.ula_step_size)
+            bwd_weight += inc_weight
+        end
+        weight_init = gen_fn.weight_init
+        weight_init *= gen_fn.weight_init_decay ^ (gen_fn.n_nmc_iters - 1)
+        for _ in gen_fn.n_nmc_iters # Apply backward NMC kernels
+            bwd_trace, inc_weight =
+                nmc_reweight(bwd_trace, selection; step_size=gen_fn.nmc_step_size,
+                            target_init=:forward, weight_init)
+            bwd_weight += inc_weight
+            weight_init /= gen_fn.weight_init_decay
+        end
+        # Evaluate probability of sampling trace under initial proposal
+        n_points = new_chosen_trace.args.n_points
+        start, stop = new_chosen_trace.args.start, new_chosen_trace.args.stop
+        delta = norm(stop .- start) / (n_points - 1)
+        mu = vec(reduce(hcat, LinRange(start, stop, n_points)[2:end-1]))
+        values = vec(bwd_trace.trajectory[:, 2:end-1])
+        init_prop_weight = logpdf(broadcasted_normal, values, mu, delta / 2)
+        # Compute importance weight for newly chosen trace
+        bwd_weight += init_prop_weight - get_score(bwd_trace)
+        new_chosen_weight = get_score(new_chosen_trace) - bwd_weight
     end
-    weight_init = 2.0 * 0.5 ^ (gen_fn.n_nmc_iters - 1)
-    for _ in gen_fn.n_nmc_iters # Apply backward NMC kernels
-        bwd_trace, inc_weight =
-            nmc_reweight(bwd_trace, selection; step_size=gen_fn.nmc_step_size,
-                         target_init=:forward, weight_init)
-        bwd_weight += inc_weight
-        weight_init /= 0.5 # Gradually change backward kernel
-    end
-    # Evaluate probability of sampling trace under initial proposal
-    n_points = new_chosen_trace.args.n_points
-    start, stop = new_chosen_trace.args.start, new_chosen_trace.args.stop
-    delta = norm(stop .- start) / (n_points - 1)
-    mu = vec(reduce(hcat, LinRange(start, stop, n_points)[2:end-1]))
-    values = vec(bwd_trace.trajectory[:, 2:end-1])
-    init_prop_weight = logpdf(broadcasted_normal, values, mu, delta / 2)
-    # Compute importance weight for newly chosen trace
-    bwd_weight += init_prop_weight - get_score(bwd_trace)
-    new_chosen_weight = get_score(new_chosen_trace) - bwd_weight
     # Rerun particle filter for other traces if arguments change
-    if all(diff isa NoChange for diff in argdiffs) || args == trace.args
+    if all(diff isa NoChange for diff in argdiffs) || all(args .== Base.values(trace.args))
         new_w_sum = exp(trace.log_z_est + log(gen_fn.n_particles)) -
                     exp(trace.chosen_weight)
     else
@@ -159,12 +171,12 @@ function Gen.update(
         pf = pf_initialize(target_fn, args, EmptyChoiceMap(), n_particles)
         # Perform NMC and ULA iterations
         selection = AllSelection()
-        weight_init = 2.0
+        weight_init = gen_fn.weight_init
         for _ in 1:gen_fn.n_nmc_iters
             pf_move_reweight!(pf, nmc_reweight, (selection,);
                               step_size=gen_fn.nmc_step_size,
                               target_init=:backward, weight_init)
-            weight_init *= 0.5 # Gradually adjust backward kernel
+            weight_init *= gen_fn.weight_init_decay
         end
         pf_move_reweight!(pf, ula_reweight, (selection, gen_fn.ula_step_size), 
                           gen_fn.n_ula_iters)
@@ -172,7 +184,7 @@ function Gen.update(
     end
     # Update estimate of log normalizing constant and normalized log probability
     new_w_sum += exp(new_chosen_weight)
-    new_log_z_est = log(new_w_sum) - log(gen_fn.n_particles)
+    new_log_z_est = log(max(new_w_sum, 0.0)) - log(gen_fn.n_particles)
     new_score = get_score(new_chosen_trace) - new_log_z_est
     # Construct updated trace and weight
     new_trace = SMCTrajectoryTrace{D}(gen_fn, new_chosen_trace, new_chosen_weight,
